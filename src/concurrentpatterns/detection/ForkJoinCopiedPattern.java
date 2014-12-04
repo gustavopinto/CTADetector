ddc7679d46c9224755ed91fbbbb9ec2c6154d5c3
Gustavo Pinto <gustavohenrique.86@gmail.com>
2014-12-04 16:29:39 -0300

    filtering out the compute method

--- src/concurrentpatterns/detection/ForkJoinCopiedPattern.java
+++ src/concurrentpatterns/detection/ForkJoinCopiedPattern.java
@@ -7,6 +7,7 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclaration;
 
@@ -21,27 +22,51 @@ public class ForkJoinCopiedPattern extends ASTVisitor {
 				
 				for(MethodDeclaration method: node.getMethods()) {
 					if(method.isConstructor()) {
-						for(Object param: method.parameters()) {
-							VariableDeclaration var = (VariableDeclaration) param;
-							ITypeBinding type = var.resolveBinding().getType();
-	                        System.out.println(var + " -> " + type);
-						}
+						analyzeConstructor(method);
 					}
 					
-					if(!method.isConstructor() && method.parameters().size() == 0 && method.getName().equals("compute")) {
-						System.out.println(method.getName());
-						method.accept(new ASTVisitor() {
-							@Override
-							public boolean visit(MethodDeclaration node) {
-								
-								return true;
-							}
-						});
+					if(!method.isConstructor() && 
+							method.parameters().size() == 0 && 
+							method.getName().getIdentifier().equals("compute")) {
+						analyzeComputeMethod(method);
 					}
 				}
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * Check pre-conditions:
+	 *  - the data structure should be copied
+	 *  - the resulting variables should be passed to a new instance of the Task class
+	 *  - the computation should be divided in two parts: 
+	 *  	- (1) verify if the computation can be solved sequentially
+	 *  	- (2) recursively create smaller parts of the computation
+	 *  
+	 * @param MethodDeclaration method
+	 */
+	private void analyzeComputeMethod(MethodDeclaration method) {
+		System.out.println(method.getName());
+		List stms = method.getBody().statements();
+		for (Object o: stms) {
+			Statement s = (Statement) o;
+			System.out.print(s);
+		}
+				
+	}
+
+	/** 
+	 * Verifies if constructor initializes the data structure var
+	 *  
+	 * @param method
+	 */
+	private void analyzeConstructor(MethodDeclaration method) {
+		for(Object param: method.parameters()) {
+			VariableDeclaration var = (VariableDeclaration) param;
+			ITypeBinding type = var.resolveBinding().getType();
+//            System.out.println(var + " -> " + type);
+		}		
 	}
 
 	private List<ITypeBinding> getDataStructureFields(TypeDeclaration node) {
